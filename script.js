65d5363f96bdea3a1e8b98be50d222cb43601dff
unknown <ashiquldeveloper@gmail.com>
2025-08-27 23:09:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const showLeftMenu = document.querySelector('.hide-details');
+const showMore = document.querySelector("#showMore");
+const hideLess = document.querySelector('#hide-less');
+
+
+showMore.addEventListener('click',()=>{
+showMore.style.display = 'none';
+showLeftMenu.style.display = 'block';
+})
+
+hideLess.addEventListener('click',()=>{
+    showLeftMenu.style.display = 'none';
+    showMore.style.display = 'block';
+
+})
+
